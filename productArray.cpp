91fbd043d73409b0b83952dd7cf3c3ae2c7c15e9
vartika06 <vartika.1998@gmail.com>
2019-08-01 05:03:36 +0530

    day 1 and day 2

---
+++ productArray.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <climits>
+#include <vector>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+//Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+//Can't use division;
+
+
+
+vector<int> productArr(vector<int>& A)
+{
+    vector<int> product(A.size());
+    int temp = 1, prod;
+    //left side
+    for(int i = 0; i<A.size(); i++)
+    {
+        product[i] = temp;
+        temp *= A[i];
+    }
+    //right side
+    temp = 1;
+    for(int i = A.size()-1; i>=0; i--)
+    {
+        product[i] *= temp;
+        temp *= A[i];
+    }
+
+    return product;
+}
+
+int main()
+{
+    vector<int> A = {1,2,3,4,5};
+    A = productArr(A);
+    for(int i = 0; i<A.size(); i++)
+        cout<<A[i]<<endl;
+
+}
---
+++ sumPair.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <climits>
+#include <vector>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+//Given a list of numbers and a number k, return count of pairs that add upto k;
+//method used - map;
+
+int countOfPairs(vector<int>& A, int sum)
+{
+    unordered_map<int, int> m;
+    for(int i = 0; i<A.size(); i++)
+        m[A[i]]++;
+    int twice_count = 0;
+    for(int i = 0; i<A.size(); i++)
+    {
+        twice_count += m[sum-A[i]];
+
+        if(sum-A[i]==A[i]) twice_count--;
+    }
+
+    return twice_count/2;
+
+}
+
+int main()
+{
+    vector<int> arr = {10, 12, 10, 15, -1, 7, 6, 5, 4, 2, 1, 1, 1};
+    int count1 = countOfPairs(arr,11); //k = 11;
+    cout<<count1<<endl;
+
+
+}
